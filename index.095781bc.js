11584e98f7d7076380c7fae29f9ff7c3b0385aac
Yaroslav Kravchuk <Yaroslav.kravchyk@gmail.com>
2026-04-24 11:19:42 +0300

    make build

---
+++ index.095781bc.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),o=e.offsetWidth,s=e.offsetHeight,f=t.offsetWidth,l=t.offsetHeight;t.style.position="absolute",t.style.left=(o-f)/2-10+"px",t.style.top=(s-l)/2-10+"px";
+//# sourceMappingURL=index.095781bc.js.map
